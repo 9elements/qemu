71691c7ac758fa73e288cd6f111ef18329161bc7
mox669 <mox669.dev@gmail.com>
2024-07-18 16:58:33 +0200

    edit fru contents for  sentinel dome eeproms

--- hw/arm/aspeed_eeprom.h
+++ hw/arm/aspeed_eeprom.h
@@ -15,11 +15,11 @@ extern const size_t tiogapass_bmc_fruid_len;
 extern const uint8_t fby35_nic_fruid[];
 extern const uint8_t fby35_bb_fruid[];
 extern const uint8_t fby35_sentinel_dome_fruid[];
-extern const uint8_t fby35_sentinel_dome_fruid_len;
 extern const uint8_t fby35_bmc_fruid[];
 extern const size_t fby35_nic_fruid_len;
 extern const size_t fby35_bb_fruid_len;
 extern const size_t fby35_bmc_fruid_len;
+extern const size_t fby35_sentinel_dome_fruid_len;
 
 extern const uint8_t yosemitev2_bmc_fruid[];
 extern const size_t yosemitev2_bmc_fruid_len;
